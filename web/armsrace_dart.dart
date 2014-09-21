0ce5e4f9139902c5eaf2c3337055c3fbff4d5f90
Sven-Erik Petermann <sverik@gmail.com>
2014-09-21 14:50:59 +0300

    Ehitise rida ja nupp.

--- web/armsrace_dart.dart
+++ web/armsrace_dart.dart
@@ -26,6 +26,17 @@ void main() {
   	ui = new UserInterface(logic, state, spec);
   	ui.moneyAmount = querySelector("#money_amount_id");
 
+  	Element econ1 = querySelector("#econ1");
+  	Element econ2 = querySelector("#econ2");
+  	Element build1 = econ1.querySelector("#buildButton");
+  	build1.onClick.listen((MouseEvent e){
+  		build1.classes.remove("enabled");
+  		build1.classes.add("disabled");
+  		econ2.classes.remove("hidden");
+  		econ2.classes.add("enabled");
+  		print("yes");
+  	});
+
   	requestTick();
 	});
 
